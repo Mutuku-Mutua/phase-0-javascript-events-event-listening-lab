902b052e0a85b12c03b6ba40eb6d4d0dc1554cb9
Mutuku Mutua <mutua.mutuku@student.moringaschool.com>
2022-12-30 16:30:17 +0300

    Copmleted assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+// }
 function addingEventListener() {
-    const input = document.getElementById('input');
-input.addEventListener('click', function() {
-  alert('I was clicked!');
-});
+  const input = document.getElementById('input') ;
+input.addEventListener('click', () => {alert("you clicked me!")})
 }
+
+addingEventListener()
